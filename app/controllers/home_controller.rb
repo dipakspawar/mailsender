3fa67bd6d6b27de31b41bb71876db0c5edb4bb9c
Dipak Pawar <dspvjti@gmail.com>
2012-02-10 12:37:47 +0530

    2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
   def send1
     Notifier.send_message('dspvjti@gmail.com').deliver
-    flash[:notice]="Message Sent"
+    flash[:notice]="Message has been Sent Successfully"
     redirect_to root_path
   end
 end
